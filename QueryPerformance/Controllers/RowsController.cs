12fe0f80d9c24fba57486d9fad6c8963c497b50a
Davi Pinto Ferreira <davipferr@gmail.com>
2025-02-02 19:15:01 -0300

    Create RowsControllers, his actions and views

---
+++ QueryPerformance/Controllers/RowsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace QueryPerformance.Controllers
+{
+    public class RowsController : Controller
+    {
+
+        public ViewResult Thousand()
+        {
+            return View();
+        }
+
+        public ViewResult TenThousand()
+        {
+            return View();
+        }
+
+        public ViewResult HundredThousand()
+        {
+            return View();
+        }
+
+        public ViewResult Million()
+        {
+            return View();
+        }
+
+    }
+}
